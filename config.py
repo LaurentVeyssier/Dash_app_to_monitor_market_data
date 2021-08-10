6d3fd8cbb79e475d1c5beaaf6a5da7f4a3edf9fd
Laurent VEYSSIER <68251051+LaurentVeyssier@users.noreply.github.com>
2021-08-10 21:58:40 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# TWITTER API
+# consumer key, consumer secret, access token, access secret.
+# section API
+con_key="your_code_here"
+con_sec="your_code_here"
+#section token, after generate step
+a_token="your_code_here"
+a_secret="your_code_here"
+
+
+# REDDIT API
+# see tutorial https://towardsdatascience.com/ultimate-beginners-guide-to-collecting-text-for-natural-language-processing-nlp-with-python-256d113e6184
+r_cid = 'your_code_here'
+r_csec ='your_code_here'
+r_uag = 'application_name_here'
